91ce6042c12172eac68283b4dec5e2a77607cf17
John Marin <jmarinsocha@hotmail.com>
2022-12-08 13:36:01 -0500

    Actualización de clases en JS según el nuevo estandar

--- JS/Car.js
+++ JS/Car.js
@@ -1,12 +1,31 @@
-function Car(license, driver) {
-    this.id;
-    this.license = license;
-    this.driver = driver;
-    this.passenger;   
-}
+//Creación de la clase con funciones en el estandar anterior de JS
 
-Car.prototype.printDataCar = function () {
-    console.log(this.driver)
-    console.log(this.driver.name)
-    console.log(this.driver.document)
+//function Car(license, driver) {
+//    this.id;
+//    this.license = license;
+//    this.driver = driver;
+//    this.passenger;   
+//}
+//
+//Car.prototype.printDataCar = function () {
+//    console.log(this.driver)
+//    console.log(this.driver.name)
+//    console.log(this.driver.document)
+//}
+
+
+//Creación de la clase con el nuev estandar de JS
+class Car {
+    constructor(license, driver) {
+        this.id;
+        this.license = license;
+        this.driver = driver;
+        this.passenger;
+    } 
+
+    printDataCar() {
+        console.log(this.driver)
+        console.log(this.driver.name)
+        console.log(this.driver.document)
+    }
 }
