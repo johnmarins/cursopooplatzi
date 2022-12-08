0fcb2e550696b630e22b03742fba7a10db18eb64
John Marin <jmarinsocha@hotmail.com>
2022-12-08 16:39:34 -0500

    Herencia aplicada en Python y JS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("Andres Herrera","AND123"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("AWD456", new Account("Andrea Ferrer", "ANDA456"), "Chevrolet", "Spark");
+uberX.passenger = 4;
+uberX.printDataCar();
